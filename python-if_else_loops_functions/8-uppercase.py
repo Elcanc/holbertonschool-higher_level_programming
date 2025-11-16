9d80c5235c891a900760b4741e9f8f3af68937ca
Elcanc <elcan.ceferov.205@gmail.com>
2025-11-16 16:03:06 +0000

    x

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def uppercase(str):
     for c in str:
-        if 97 <= ord(c) <= 122:  # kiçik hərf
-            print("{}".format(chr(ord(c) - 32)), end="")
-        else:
-            print("{}".format(c), end="")
+        print("{}".format(chr(ord(c) - 32) if 97 <= ord(c) <= 122 else c), end="")
+    print()
