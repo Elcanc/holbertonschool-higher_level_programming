bca13dfdd6ba762ef60f7ff20168109904d51148
Elcanc <elcan.ceferov.205@gmail.com>
2025-11-18 12:26:30 +0000

    x

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+args = sys.argv[1:]
+a = len(args)
+if a == 1:
+    print("{} argument:".format(a))
+elif a == 0:
+    print("{} arguments.".format(a))
+else:
+    print("{} arguments:".format(a))
+for i, arg in enumerate(args, 1):
+    print("{}: {}".format(i, arg))
