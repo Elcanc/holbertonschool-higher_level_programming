8f2d8357de685cdaa775d7220a30c60c138be7d3
Elcanc <elcan.ceferov.205@gmail.com>
2025-11-19 13:10:14 +0000

    x

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,8 +2,8 @@
 def print_matrix_integer(matrix=[[]]):
     for a in matrix:
         for b in range(len(a)):
-            if i != a[-1]:
+            if b != len(a) - 1:
                 print("{:d}".format(a[b]), end=' ')
-            elif i == a[-1]:
+            elif b == len(a) - 1:
                 print("{:d}".format(a[b]), end='')
     print('--')
