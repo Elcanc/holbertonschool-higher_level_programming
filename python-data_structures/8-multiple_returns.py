967f59b2a43871b3e68bd1b14596d7d6293afa34
Elcanc <elcan.ceferov.205@gmail.com>
2025-11-21 09:49:47 +0000

    x

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -2,7 +2,7 @@
 def multiple_returns(sentence):
     length = len(sentence)
     first = sentence[0]
-    if x == 0:
+    if length == 0:
         return None
     else:
         return "Length: {:d} - First character: {}".format(length, first)
