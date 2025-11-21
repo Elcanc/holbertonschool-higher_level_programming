99f62caa9a4df9c803c488ed779e24cbcad49464
Elcanc <elcan.ceferov.205@gmail.com>
2025-11-21 10:06:41 +0000

    x

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,2 +1,8 @@
 #!/usr/bin/python3
-
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
+    else:
+        x = my_list.sort()
+        max_value = x[0]
+        return max_value
