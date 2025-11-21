94239cce12b2f213200a5f805ed0b94914146a1a
Elcanc <elcan.ceferov.205@gmail.com>
2025-11-21 09:48:54 +0000

    x

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,2 +1,8 @@
 #!/usr/bin/python3
-
+def multiple_returns(sentence):
+    length = len(sentence)
+    first = sentence[0]
+    if x == 0:
+        return None
+    else:
+        return "Length: {:d} - First character: {}".format(length, first)
