c3fbfb41c49ca7a0584e8ef4075ccb4597b0f950
Elcanc <elcan.ceferov.205@gmail.com>
2025-11-22 09:54:36 +0000

    x

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -1,2 +1,8 @@
 #!/usr/bin/python3
-
+def only_diff_elements(set_1, set_2):
+    sett = []
+    for i in set_1:
+        for j in set_2:
+            if i != j:
+                sett.append(i)
+    return sett
