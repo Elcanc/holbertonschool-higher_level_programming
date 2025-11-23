a413e4331a53f6f988b842ccb557e707ba09d6b7
Elcanc <elcan.ceferov.205@gmail.com>
2025-11-23 12:25:38 +0000

    x

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -4,8 +4,8 @@ def best_score(a_dictionary):
     for i in a_dictionary:
         if isinstance(a_dictionary[i], int):
             list.append(a_dictionary[i])
-    new = sorted(list)
-    a = new[-1]
+            new = sorted(list)    
+            a = new[-1]
     for key, value in a_dictionary.items():
         if value == a:
             best_key = key
