f650fa1142e752c041ee1c90402cc31bd4eda4fa
Elcanc <elcan.ceferov.205@gmail.com>
2025-11-23 13:37:11 +0000

    x

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string): 
     dic = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000, "IV": 4, "IX": 9, "XL": 40, "XC": 90, "CD": 400, "CM": 900}
+    if isinstance(roman_string, int):
+        return 0
     a = 0
     r = 0
     n = len(roman_string)
-    if isinstance(roman_string, int):
-        return 0
     while a < n:
         if a + 1 < n and roman_string[a: a+2] in dic:
             r += dic[roman_string[a: a+2]]
