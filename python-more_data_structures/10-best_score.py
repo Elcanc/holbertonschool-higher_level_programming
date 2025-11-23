c3f904d26cba9f22c2d7840e7f4a8c96a79414cc
Elcanc <elcan.ceferov.205@gmail.com>
2025-11-23 12:30:03 +0000

    x

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    list = []
-    best_key = None
-    for i in a_dictionary:
-        if isinstance(a_dictionary[i], int):
-            list.append(a_dictionary[i])
-            new = sorted(list)    
-            a = new[-1]
-    for key, value in a_dictionary.items():
-        if value == a:
-            best_key = key
-    return best_key
+    if a_dictionary is None:
+        return None
+    else:
+        list = []
+        best_key = None
+        for i in a_dictionary:
+            if isinstance(a_dictionary[i], int):
+                list.append(a_dictionary[i])
+                new = sorted(list)    
+                a = new[-1]
+        for key, value in a_dictionary.items():
+            if value == a:
+                best_key = key
+        return best_key
