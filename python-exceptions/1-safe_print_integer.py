720d78dc0b7eb1b038d5f999dedda41a9fef5996
Elcanc <elcan.ceferov.205@gmail.com>
2025-11-23 15:11:19 +0000

    x

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        return '{:d}'.format(value)
-    except ValueError:
-        return value
+        print('{:d}'.format(value))
+    except (ValueError, TypeError):
+        return False
