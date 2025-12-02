cc77a6845ae59a88ede3065eaa5bd362ade164c8
Elcanc <elcan.ceferov.205@gmail.com>
2025-12-02 13:14:15 +0000

    x

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -4,15 +4,18 @@ class Rectangle:
     """Defines a rectangle."""
 
     def __init__(self, width=0, height=0):
+        """Init function"""
         self.width = width
         self.height = height
 
     @property
     def width(self):
+        """getter for width"""
         return self.__width
 
     @width.setter
     def width(self, value):
+        """setter for width"""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
@@ -21,10 +24,12 @@ class Rectangle:
 
     @property
     def height(self):
+        """getter for height"""
         return self.__height
 
     @height.setter
     def height(self, value):
+        """setter for height"""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
