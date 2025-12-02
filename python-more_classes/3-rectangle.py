74e33b90744322f0634203d73008f4fe5e2a94e2
Elcanc <elcan.ceferov.205@gmail.com>
2025-12-02 13:33:54 +0000

    x

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -5,6 +5,7 @@
 class Rectangle:
     """Defines a rectangle with width and height, with area and perimeter."""
 
+
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
