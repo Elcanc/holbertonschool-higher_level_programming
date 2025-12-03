fb866120d9f0764186ddc82ebf9352720376a7bb
Elcanc <elcan.ceferov.205@gmail.com>
2025-12-03 12:43:02 +0000

    x

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -3,6 +3,8 @@
 
 
 class MyList(list):
+    """Inherits from list and adds a method to print the list sorted"""
+
     def print_sorted(self):
         """ Inherits from list and adds a method to print sorted list. """
 
