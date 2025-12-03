ddeed27f7b88e9d42b3694dcbb85f011a68fdcd7
Elcanc <elcan.ceferov.205@gmail.com>
2025-12-03 11:28:28 +0000

    x

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -113,7 +113,7 @@ class Rectangle:
     def __str__(self):
         if self.__width == 0 or self.__height == 0:
             return ""
-        lines = [str(print_symbol) * self.__width for _ in range(self.__height)]
+        lines = [str(self.print_symbol) * self.__width for _ in range(self.__height)]
         return "\n".join(lines)
 
     def __repr__(self):
