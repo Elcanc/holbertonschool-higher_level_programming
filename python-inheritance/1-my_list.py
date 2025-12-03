48fa8d3eade51dd3159ee154cafffc77af7e9616
Elcanc <elcan.ceferov.205@gmail.com>
2025-12-03 12:44:47 +0000

    x

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -8,7 +8,4 @@ class MyList(list):
     def print_sorted(self):
         """ Inherits from list and adds a method to print sorted list. """
 
-        def print_sorted(self):
-            """Prints the list in ascending order."""
-
-            print(sorted(self))
+        print(sorted(self))
