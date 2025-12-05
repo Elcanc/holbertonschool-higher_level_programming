da154d77328295d553adc615673973e491188b44
Elcanc <elcan.ceferov.205@gmail.com>
2025-12-05 11:44:44 +0000

    x

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -14,7 +14,6 @@ class Student:
         """Retrieve a dict representation of Student."""
         if (isinstance(attrs, list) and
             all(isinstance(a, str) for a in attrs)):
-
             return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
 
         return self.__dict__
