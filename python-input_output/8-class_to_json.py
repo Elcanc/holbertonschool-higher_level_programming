7c7303cadebfcd0661d34988a9e1f55ae55692aa
Elcanc <elcan.ceferov.205@gmail.com>
2025-12-05 09:41:53 +0000

    x

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,2 +1,8 @@
 #!/usr/bin/python3
-x
+"""Module that defines a function
+to convert a class instance to a dictionary"""
+
+
+def class_to_json(obj):
+    """Returns the dictionary description of an object for JSON serialization"""
+    return obj.__dict__
