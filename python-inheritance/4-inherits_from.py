aca729912fe83c9a232ec25fb59d561a3043f524
Elcanc <elcan.ceferov.205@gmail.com>
2025-12-05 06:38:41 +0000

    x

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""Returns True if obj is instance of a class that inherited from a_class"""
+
+
 def inherits_from(obj, a_class):
     """Returns True if obj is instance of a class that inherited from a_class"""
     return isinstance(obj, a_class) and type(obj) is not a_class
