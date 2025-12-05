11d8bb53c4bbab34e029f916cf74ca9a8795bdb2
Elcanc <elcan.ceferov.205@gmail.com>
2025-12-05 11:42:35 +0000

    x

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -1,2 +1,25 @@
 #!/usr/bin/python3
-x
+"""Student class module"""
+
+
+class Student:
+    """Student class that defines a student."""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieve a dict representation of Student."""
+        if (isinstance(attrs, list) and
+            all(isinstance(a, str) for a in attrs)):
+
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """Replace all attributes of the Student instance."""
+        for key, value in json.items():
+            setattr(self, key, value)
