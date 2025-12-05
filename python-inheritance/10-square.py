188e88be965ca6f24eaf0f255752976cc767920b
Elcanc <elcan.ceferov.205@gmail.com>
2025-12-05 08:22:16 +0000

    x

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""square"""
+
+
+Rectangle = __import__('9-rectangle.py').Rectangle
+
+
+class Square(Rectangle):
+    """Square class inherits from Rectangle"""
+
+    def __init__(self, size):
+        self.__size = size
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+
+    def area(self):
+        """Return the area of the square"""
+        return self.__size ** 2
