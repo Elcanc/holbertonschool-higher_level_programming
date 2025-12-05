08840314e05aeb3eee819eb1be5e2afa9cb25bcd
Elcanc <elcan.ceferov.205@gmail.com>
2025-12-05 09:05:09 +0000

    x

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -4,6 +4,7 @@ Module that defines a function
 to append a string to a UTF-8 text file
 """
 
+
 def append_write(filename="", text=""):
     """Appends a string to a UTF-8 text file
     and returns the number of characters added"""
