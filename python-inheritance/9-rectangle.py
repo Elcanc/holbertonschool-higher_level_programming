24fc76d78158c0af3bae3673bcf4273f35d90ead
Elcanc <elcan.ceferov.205@gmail.com>
2025-12-05 08:09:30 +0000

    x

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+"""rectangle"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """rectangle that inherits from BaseGeometry"""
+
+    def __init__(self, width, height):
+        self.__width = width
+        self.__height = height
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
+    def area(self):
+        """Returns area of the rectangle"""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """String representation"""
+        return f"[Rectangle] {self.__width}/{self.__height}"
