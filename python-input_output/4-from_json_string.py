8a77366992e8647d9b4a9a79e5d4a33594dbb58f
Elcanc <elcan.ceferov.205@gmail.com>
2025-12-05 09:19:53 +0000

    x

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
-"""Module that defines a function to convert a JSON string to a Python object"""
+"""
+Module that defines a
+function to convert a JSON string to a Python object
+"""
 
 
 import json
 
 
 def from_json_string(my_str):
-    """Returns the Python object represented by a JSON string"""
+    """
+    Returns the Python object
+    represented by a JSON string
+    """
     return json.loads(my_str)
