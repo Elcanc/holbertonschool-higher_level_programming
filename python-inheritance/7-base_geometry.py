2e7419f2fa49de20641b953d371dac531db2004a
Elcanc <elcan.ceferov.205@gmail.com>
2025-12-05 06:51:44 +0000

    x

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+"""empty class"""
+
+
+class BaseGeometry:
+    """Empty class"""
+    pass
+
+    def area(self):
+        """raises exception"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validates that value is a positive integer"""
+        if type(value) is not int:
+            raise TypeError(f"{name} must be an integer")
+
+        if type(value) <= 0:
+            raise ValueError(f"{name} must be greater than 0")
