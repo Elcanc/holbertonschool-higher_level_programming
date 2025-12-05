c01e4e196c72b44a569c3acc1a788d6d9920db3c
Elcanc <elcan.ceferov.205@gmail.com>
2025-12-05 09:04:33 +0000

    x

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -5,6 +5,7 @@ to append a string to a UTF-8 text file
 """
 
 def append_write(filename="", text=""):
-    """Appends a string to a UTF-8 text file and returns the number of characters added"""
+    """Appends a string to a UTF-8 text file
+    and returns the number of characters added"""
     with open(filename, "a", encoding="utf-8") as f:
         return f.write(text)
