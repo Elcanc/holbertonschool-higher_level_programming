27ac54bf17fefadc3dd35944e95c4d07aebcf169
Elcanc <elcan.ceferov.205@gmail.com>
2025-12-05 11:48:35 +0000

    x

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -11,9 +11,17 @@ class Student:
         self.age = age
 
     def to_json(self, attrs=None):
-        """Retrieve a dict representation of Student."""
-        if (isinstance(attrs, list) and all(isinstance(a, str) for a in attrs)):
-            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+    """Retrieve a dict representation of Student."""
+    if (
+        isinstance(attrs, list)
+        and all(isinstance(a, str) for a in attrs)
+    ):
+        return {
+            k: getattr(self, k)
+            for k in attrs
+            if hasattr(self, k)
+        }
+
 
         return self.__dict__
 
