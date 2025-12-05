f513483e388bdc14220c2b60577f82757105070e
Elcanc <elcan.ceferov.205@gmail.com>
2025-12-05 09:42:31 +0000

    x

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -4,5 +4,6 @@ to convert a class instance to a dictionary"""
 
 
 def class_to_json(obj):
-    """Returns the dictionary description of an object for JSON serialization"""
+    """Returns the dictionary description of
+    an object for JSON serialization"""
     return obj.__dict__
