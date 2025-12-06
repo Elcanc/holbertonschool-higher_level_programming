4ae6ffdea84cd6194f9ed96ccc0e14856b12ea2d
Elcanc <elcan.ceferov.205@gmail.com>
2025-12-06 15:47:25 +0000

    x

--- SQL_more_queries/100-not_my_genres.sql
+++ SQL_more_queries/100-not_my_genres.sql
@@ -1,11 +1,8 @@
 --comment
-SELECT name
+SELECT tv_genres.name
 FROM tv_genres
-WHERE id NOT IN (
-    SELECT genre_id
-    FROM tv_show_genres
-    WHERE show_id = (
-        SELECT id FROM tv_shows WHERE title = 'Dexter'
-    )
-)
-ORDER BY name ASC;
+LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+LEFT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+    AND tv_shows.title = 'Dexter'
+WHERE tv_shows.id IS NULL
+ORDER BY tv_genres.name ASC;
