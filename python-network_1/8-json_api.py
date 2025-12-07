c1233c23ee8ad9dce060965e0601bb17557a4ee3
Elcanc <elcan.ceferov.205@gmail.com>
2025-12-07 08:44:38 +0000

    x

--- python-network_1/8-json_api.py
+++ python-network_1/8-json_api.py
@@ -1,2 +1,24 @@
 #!/usr/bin/python3
-x
+"""
+Sends a POST request to http://0.0.0.0:5000/search_user
+with the letter provided as q and displays the result.
+"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    # Arqument yoxlanması, əgər verilən yoxdursa q=""
+    q = sys.argv[1] if len(sys.argv) > 1 else ""
+
+    data = {"q": q}
+
+    try:
+        response = requests.post("http://0.0.0.0:5000/search_user", data=data)
+        json_data = response.json()  # JSON çevirməyə çalışır
+
+        if json_data:
+            print(f"[{json_data.get('id')}] {json_data.get('name')}")
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
