19ec7c91c85b1db6227ceb833a9183f6ea241f1d
Elcanc <elcan.ceferov.205@gmail.com>
2025-12-07 08:43:18 +0000

    x

--- python-network_1/7-error_code.py
+++ python-network_1/7-error_code.py
@@ -1,2 +1,17 @@
 #!/usr/bin/python3
-x
+"""
+Sends a request to a URL and displays the body of the response.
+If the HTTP status code is >= 400, displays Error code.
+"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    response = requests.get(url)
+
+    if response.status_code >= 400:
+        print("Error code:", response.status_code)
+    else:
+        print(response.text)
