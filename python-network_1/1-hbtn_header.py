0d72471d6c989988396b2205957fab4cb18da84a
Elcanc <elcan.ceferov.205@gmail.com>
2025-12-07 08:26:03 +0000

    x

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
+"""
+Sends a request to a URL and displays the value of the X-Request-Id header.
+"""
 import urllib.request
 import sys
 
-url = sys.argv[1]
+if __name__ == "__main__":
+    url = sys.argv[1]
 
-with urllib.request.urlopen(url) as response:
-    headers = response.headers
-    print(headers.get("X-Request-Id"))
+    with urllib.request.urlopen(url) as response:
+        print(response.headers.get("X-Request-Id"))
