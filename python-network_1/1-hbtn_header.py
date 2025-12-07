571c26e8dffce4dae256be18699122c2e2bee288
Elcanc <elcan.ceferov.205@gmail.com>
2025-12-07 08:23:04 +0000

    x

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+import urllib.request
+import sys
+
+url = sys.argv[1]
+
+with urllib.request.urlopen(url) as response:
+    headers = response.headers
+    print(headers.get("X-Request-Id"))
