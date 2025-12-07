85e9a68e8e0d715ef459422d82fa669a469ebf41
Elcanc <elcan.ceferov.205@gmail.com>
2025-12-07 08:46:07 +0000

    x

--- python-network_1/10-my_github.py
+++ python-network_1/10-my_github.py
@@ -1,2 +1,20 @@
 #!/usr/bin/python3
-x
+"""
+Takes GitHub username and personal access token,
+uses the GitHub API to display your user id.
+"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    token = sys.argv[2]
+
+    url = "https://api.github.com/user"
+    response = requests.get(url, auth=(username, token))
+
+    try:
+        json_data = response.json()
+        print(json_data.get("id"))
+    except ValueError:
+        print(None)
