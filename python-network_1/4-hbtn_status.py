6742401c614eb7abfde3273566fdfce2d996d7f9
Elcanc <elcan.ceferov.205@gmail.com>
2025-12-07 08:39:44 +0000

    x

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,2 +1,15 @@
 #!/usr/bin/python3
-x
+"""
+Fetches https://intranet.hbtn.io/status and displays the body of the response.
+"""
+import requests
+
+if __name__ == "__main__":
+    url = "https://intranet.hbtn.io/status"
+
+    response = requests.get(url)
+    body = response.text
+
+    print("Body response:")
+    print("\t- type:", type(body))
+    print("\t- content:", body)
