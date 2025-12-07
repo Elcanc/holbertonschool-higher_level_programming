aabc653f72c29364c9291d644a62f663c064a75d
Elcanc <elcan.ceferov.205@gmail.com>
2025-12-07 08:38:27 +0000

    x

--- python-network_1/3-error_code.py
+++ python-network_1/3-error_code.py
@@ -1,2 +1,18 @@
 #!/usr/bin/python3
-x
+"""
+Sends a request to a URL and displays the body of the response.
+Handles HTTPError and prints the error code if it occurs.
+"""
+import urllib.request
+import urllib.error
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    try:
+        with urllib.request.urlopen(url) as response:
+            body = response.read()
+            print(body.decode("utf-8"))
+    except urllib.error.HTTPError as e:
+        print("Error code:", e.code)
